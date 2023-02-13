c389a9b0c503366d93e5d3609f40a8c3090e9337
insane <rarestp2@gmail.com>
2023-02-13 20:06:59 +0000

    changes

--- pages/3D/script.js
+++ pages/3D/script.js
@@ -1,29 +1,33 @@
 console.log(document.body);
 
-
 const loader = new THREE.GLTFLoader();
 
-loader.load( '/models/jimmy.glb', function ( gltf ) {
-  //scale the model to 10x its original size
-  const multiplier = 30;
-  gltf.scene.scale.set(multiplier, multiplier, multiplier);
-	scene.add( gltf.scene );
+loader.load(
+  "/models/jimmy.glb",
+  function (gltf) {
+    //scale the model to 10x its original size
+    const multiplier = 30;
+    gltf.scene.scale.set(multiplier, multiplier, multiplier);
+    scene.add(gltf.scene);
+  },
 
-}, undefined, function ( error ) {
-
-	console.error( error );
-} );
+  function (xhr) {
+    document.innerHTML += (xhr.loaded / xhr.total) * 100 + "% loaded";
+  },
+  function (error) {
+    console.error(error);
+  }
+);
 
 var width = window.innerWidth,
-    height = window.innerHeight;
-
+  height = window.innerHeight;
 
 // Create a renderer and add it to the DOM.
 var renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
 renderer.setSize(width, height);
 
 document.body.appendChild(renderer.domElement);
-// Create the scene 
+// Create the scene
 var scene = new THREE.Scene();
 // Create a camera
 var camera = new THREE.PerspectiveCamera(45, width / height, 0.1, 10000);
@@ -35,18 +39,17 @@ scene.add(camera);
 
 // Create a light, set its position, and add it to the scene.
 var light = new THREE.PointLight(0xffffff);
-light.position.set(-100,200,100);
+light.position.set(-100, 200, 100);
 scene.add(light);
 
 // Create a light, set its position, and add it to the scene.
 var light = new THREE.PointLight(0xffffff);
-light.position.set(100,-200,100);
+light.position.set(100, -200, 100);
 scene.add(light);
 
 //world light
-var light = new THREE.AmbientLight( 0x404040 ); // soft white light
-scene.add( light );
-
+var light = new THREE.AmbientLight(0x404040); // soft white light
+scene.add(light);
 
 // Add OrbitControls so that we can pan around with the mouse.
 var controls = new THREE.OrbitControls(camera, renderer.domElement);
@@ -55,30 +58,29 @@ var controls = new THREE.OrbitControls(camera, renderer.domElement);
 // var axes = new THREE.AxisHelper(50);
 // scene.add( axes );
 
-var geometry = new THREE.BoxGeometry(5,5,5);
-var cubeMaterial = new THREE.MeshNormalMaterial(); 
+var geometry = new THREE.BoxGeometry(5, 5, 5);
+var cubeMaterial = new THREE.MeshNormalMaterial();
 
-var mesh = new THREE.Mesh( geometry, cubeMaterial );
-// scene.add( mesh ); 
+var mesh = new THREE.Mesh(geometry, cubeMaterial);
+// scene.add( mesh );
 
 resize();
 animate();
-window.addEventListener('resize',resize);
+window.addEventListener("resize", resize);
 
-function resize(){
+function resize() {
   let w = window.innerWidth;
   let h = window.innerHeight;
-  
-  renderer.setSize(w,h);
+
+  renderer.setSize(w, h);
   camera.aspect = w / h;
   camera.updateProjectionMatrix();
 }
 
 // Renders the scene
 function animate() {
-
-  renderer.render( scene, camera );
+  renderer.render(scene, camera);
   controls.update();
 
-  requestAnimationFrame( animate );
+  requestAnimationFrame(animate);
 }
